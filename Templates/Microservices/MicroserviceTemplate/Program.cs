d6fd0c34f107aa5c9a06cfc5da59630dabe294f1
Joni Collinge <jonathan-collinge@hotmail.com>
2016-01-15 19:51:07 +0000

    Updated to ASPNET5 reference project

---
+++ Templates/Microservices/MicroserviceTemplate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroserviceTemplate
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var fabricRuntime = FabricRuntime.Create())
+            {
+                fabricRuntime.RegisterServiceType("MicroserviceTemplateType", typeof(MicroserviceTemplate));
+
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+    }
+}
--- Templates/Microservices/MicroserviceTemplate/Startup.cs
+++ Templates/Microservices/MicroserviceTemplate/Startup.cs
@@ -6,29 +6,38 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace MicroserviceTemplate
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                                                      .AddEnvironmentVariables()
+                                                      .Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //// Add framework services.
+            //services.AddMvcCore()
+            //        .AddJsonFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
+            app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello from gateway!");
             });
         }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
