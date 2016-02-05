c550c80b11e95c100307809e1f63e07b244a0603
Lawrence Gripper <info@grippers.co.uk>
2016-02-05 13:57:26 -0800

    Converted deployment service to ASPNET RC2 and fixed naming service issue. Testing still required.

---
+++ Agents/Deployment/ServiceDeployer/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceDeployer
+{ 
+    public static class Program
+    {
+        private const string LocalListeningAddress = "http://+:8284";
+
+        public static void Main(string[] args)
+        {
+            if (Environment.GetEnvironmentVariable("ASPNET_ENV") == "Debug")
+            {
+                try
+                {
+                    // Build an ASP.NET 5 web application that serves as the communication listener.
+                    var webApp = new WebApplicationBuilder().UseConfiguration(WebApplicationConfiguration.GetDefault())
+                                                            .ConfigureLogging(factory =>
+                                                            {
+                                                                factory.AddConsole();
+                                                            })
+                                                            .UseStartup<Startup>()
+                                                            .Build();
+
+                    webApp.GetAddresses().Clear();
+                    webApp.GetAddresses().Add(LocalListeningAddress);
+                    Console.WriteLine(LocalListeningAddress);
+                    webApp.Run();
+                    Thread.Sleep(Timeout.Infinite);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Crashed: {0}", ex);
+                    Console.ReadLine();
+                }
+
+            }
+
+            using (var fabricRuntime = FabricRuntime.Create())
+            {
+                fabricRuntime.RegisterServiceType("ServiceDeployerType", typeof(ServiceDeployer));
+
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+    }
+}
--- Agents/Deployment/ServiceDeployer/Startup.cs
+++ Agents/Deployment/ServiceDeployer/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNet.Hosting.Internal;
 
 namespace ServiceDeployer
 {
@@ -34,16 +35,11 @@ namespace ServiceDeployer
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
 
-            app.UseIISPlatformHandler();
-
-            app.UseStaticFiles();
+            //app.UseStaticFiles();
 
             app.UseMvc();
         }
 
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
