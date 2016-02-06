a938dc761b5f5dda7469f48b773038b61573eb58
nicktrog <ntrogh@hotmail.com>
2016-02-06 09:40:09 -0800

    Added project validation

--- Tools/BlankyVSCE/src/extension.ts
+++ Tools/BlankyVSCE/src/extension.ts
@@ -40,6 +40,7 @@ class Blanky {
     private _keepZip: boolean = false;
     private _deploymentEndpoint: string;
     private _zipExists: boolean = false;
+    private _simulateDeploy: boolean = false;
 
     
     public constructor() {
@@ -49,17 +50,36 @@ class Blanky {
     public Deploy()
     {
         this.ValidateWorkspace();
-        this.GetConfigSettings();
-        this.TryRemoveZipFile();        
-        this.CreateZipFile();
+        // this.GetConfigSettings();
+        // this.TryRemoveZipFile();        
+        // this.CreateZipFile();
         // this.InvokeDeploymentService();
         // this.Cleanup();
     }   
     
     // Validate that we have a microservice project   
     private ValidateWorkspace() {
+        
+        var that = this;
+        
         // Check we have a manifest file
-        // Check that manifest is well formatted
+        var manifestPath: string = path.join(vscode.workspace.rootPath, 'ApplicationManifest.xml');
+        
+        if (!fs.existsSync(manifestPath)) {
+            vscode.window.showErrorMessage('Not a valid project: ApplicationManifest.xml file is missing.');
+            console.error('ApplicationManifest file is missing.');
+        } else {
+            vscode.workspace.findFiles("**/ServiceManifest.xml", null).then(function (results) {
+                if (results.length===0) {
+                    vscode.window.showErrorMessage('Not a valid project: ServiceManifest.xml file is missing.');
+                    console.error('ServiceManifest file is missing.');
+                } else {
+                    that.GetConfigSettings();
+                    that.TryRemoveZipFile();        
+                    that.CreateZipFile();
+                }
+            });
+        }                
     }
     
     // Zipping up all files in the current folder        
@@ -83,24 +103,20 @@ class Blanky {
     } 
     
     private TryRemoveZipFile() {
-        fs.exists(this._zipFilepath, (exists) => {
-            if (exists) {
-                console.log ('Cleaning up existing zip file.');
-                fs.unlink(this._zipFilepath);
-            }
-        });        
+        if (fs.existsSync(this._zipFilepath)) {
+           console.log ('Cleaning up existing zip file.');
+           fs.unlinkSync(this._zipFilepath);
+        };
     }
     
     private GetConfigSettings() {
-        console.log ('Getting keepZip endpoint from config.');
-        
         let config = vscode.workspace.getConfiguration("blanky");             
-        let keepZip = config.get("keepzip") as boolean;
-        this._keepZip = keepZip;
-         
-        console.log ('Keepzip config setting: ' + keepZip);
+
+        this._keepZip = config.get("keepzip") as boolean;        
+        this._simulateDeploy = config.get("simulatedeploy") as boolean;
         
-        console.log ('Getting deployment endpoint from config.');
+        console.log ('Keepzip config setting: ' + this._keepZip);
+        console.log ('SimulateDeploy config setting: ' + this._simulateDeploy);
         
         let endpoint = config.get("deploymentendpoint") as string;
         
@@ -116,30 +132,42 @@ class Blanky {
     
     // Invoke the Blanky deployment endpoint to deploy the micro service
     private InvokeDeploymentService() {
-        var that = this;
-                
-        console.log('Starting deployment to ' + this._deploymentEndpoint);
-        vscode.window.showInformationMessage("Deploying to " + this._deploymentEndpoint);
-        
-        // Invoke the deployment endpoint rest service
-        var req = request({
-            uri: this._deploymentEndpoint,
-            method: "POST",
-            timeout: 900000},
-            function (err, resp, body) {
-                if (err) {
-                    console.log(err);
-                    vscode.window.showErrorMessage('Deploying to Blanky failed:' + err);
-                } else {
-                    console.log('Server response: ' + body);
-                    vscode.window.showInformationMessage('Deploying to Blanky completed succesfully!');
-                }
-                that.Cleanup();
-            }
-            );
+        if (!this._simulateDeploy) {            
+            var that = this;
+                    
+            console.log('Starting deployment to ' + this._deploymentEndpoint);
+            vscode.window.showInformationMessage("Deploying to " + this._deploymentEndpoint);
+            
+            // Invoke the deployment endpoint rest service
+            var req = request({
+                uri: this._deploymentEndpoint,
+                method: "POST",
+                timeout: 900000},
+                function (err, resp, body) {
+                    if (err) {
+                        console.log(err);
+                        vscode.window.showErrorMessage('Deploying to Blanky failed:' + err);
+                    } else {
+                        console.log('Server response: ' + body);
+                        vscode.window.showInformationMessage('Deploying to Blanky completed succesfully!');
+                    }
 
-        var form = req.form();
-        form.append('file', fs.createReadStream(this._zipFilepath));        
+                    that.Cleanup();
+                    
+                    console.log('Blanky deployment completed');
+                }
+                );
+
+            var form = req.form();
+            form.append('file', fs.createReadStream(this._zipFilepath));        
+        } else {
+            console.log('Simulating deployment to ' + this._deploymentEndpoint);
+            that.Cleanup();
+
+            vscode.window.showInformationMessage('Deploying to Blanky completed succesfully!');
+
+            console.log('Blanky deployment completed');
+        }
     }
     
     private Cleanup() {
