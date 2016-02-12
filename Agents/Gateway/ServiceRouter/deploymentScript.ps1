c2aa11f161d17eb0085190bc1bd0dc5169f79de9
Lawrence Gripper <info@grippers.co.uk>
2016-02-12 15:03:31 +0000

    Updated extension to show user output in VSCode and tweaked deployment script

--- Agents/Gateway/ServiceRouter/deploymentScript.ps1
+++ Agents/Gateway/ServiceRouter/deploymentScript.ps1
@@ -42,28 +42,28 @@ function Get-ImageStoreConnectionStringFromClusterManifest
 #Store\redishost
 
 ##Update this command to deploy to remote cluster as needed. 
-Write-Verbose 'Connecting to cluster...'
+Write-Verbose "Connecting to cluster..."
 Connect-ServiceFabricCluster
 
-Write-Verbose 'Successfully connected!'
+Write-Verbose "Successfully connected!"
 
-Write-Verbose 'Copying application package...'
+Write-Verbose "Copying application package..."
 $clusterManifestText = Get-ServiceFabricClusterManifest
 $imageStoreConnectionString = Get-ImageStoreConnectionStringFromClusterManifest ([xml] $clusterManifestText)
 ##When deploying to azure cluster remove the imagestoreconnectionstring parameter
 Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $appPackagePath -ImageStoreConnectionString $imageStoreConnectionString -ApplicationPackagePathInImageStore $appImageStoreName
-Write-Verbose 'Successfully copied app!'
+Write-Verbose "Successfully copied app!"
 
-Write-Verbose 'Test the application package...'
+Write-Verbose "Test the application package..."
 Test-ServiceFabricApplicationPackage $appPackagePath -ImageStoreConnectionString $imageStoreConnectionString -Verbose
-#Write-Verbose 'Successfully registered app!'
+#Write-Verbose "Successfully registered app!"
 
 ##For testing - clean up any existing deployments
 Write-Verbose "Cleaning up existing deployments of $appName"
-$service = get-ServiceFabricApplication -ApplicationName "fabric:/$appName"
+$service = Get-ServiceFabricApplication -ApplicationName "fabric:/$appName"
 if ($service)
 {
-    Write-Verbose '-- Removing app $($service.ApplicationName)'
+    Write-Verbose "-- Removing app $($service.ApplicationName)"
     remove-servicefabricapplication $service.ApplicationName -force
 }
 
@@ -72,16 +72,16 @@ if ($types)
 {
     foreach ($type in $types)
     {
-        Write-Verbose '-- Removing type $($service.ApplicationName)'
+        Write-Verbose "-- Removing type $($service.ApplicationName)"
         Unregister-ServiceFabricApplicationType $type.ApplicationTypeName -ApplicationTypeVersion  $type.ApplicationTypeVersion -force
     }
 }
 
 ##Now lets register it again and deploy
-Write-Verbose 'Registering the application'
+Write-Verbose "Registering the application"
 Register-ServiceFabricApplicationType -ApplicationPathInImageStore $appImageStoreName
-Write-Verbose 'Successfully registered!'
+Write-Verbose "Successfully registered!"
 
-Write-Verbose 'creating new app instance!'
+Write-Verbose "creating new app instance!"
 New-ServiceFabricApplication -ApplicationName $appName -ApplicationTypeName $appType -ApplicationTypeVersion 1.0.0.3
 
